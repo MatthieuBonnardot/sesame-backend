2bd0423b9dd4e2c7171b65dafafb648383d66a73
Peter French <peterfrench94@gmail.com>
2021-02-01 10:18:18 +0100

    feat: still configuring env

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-module.exports = {};
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  MONGO_URI = process.env.MONGO_DB_URI_TEST1 || `mongodb+srv://aas:codeworks@aas.pwwb5.mongodb.net/sesame`
+
+};
