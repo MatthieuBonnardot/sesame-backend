ca37c3a0e939c5cebcfc36f0ca6c32856ab54bf0
Peter French <peterfrench94@gmail.com>
2021-02-01 10:49:00 +0100

    fix: fix exposing db path to git

---
+++ server/src/Models/Sequelize/index.models.ts
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+
+const sequelize = new Sequelize('sesame-test', 'peter', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false,
+});
+
+const db = {};
+
+const allFiles = fs.readdirSync(__dirname);
+const filteredFiles = allFiles.filter((file) => file.indexOf('.') !== 0 && file !== 'index.model.js' && file.slice(-3) === '.js' );
+
+filteredFiles.forEach(file => {
+  const model = require(path.join(__dirname,file))(sequelize, Sequelize.DataTypes);
+  db[model.name] = model;
+});
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
