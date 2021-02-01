83a253b8fe7833d4a621e1d5cc109c40ce5b26b7
Peter French <peterfrench94@gmail.com>
2021-02-01 20:17:09 +0100

    feat: linked routes to controllers

--- server/dist/Models/Sequelize/permissions.model.js
+++ server/dist/Models/Sequelize/permissions.model.js
@@ -1,1 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Permissions {
+}
+exports.default = Permissions;
+Permissions.init({});
 //# sourceMappingURL=permissions.model.js.map
