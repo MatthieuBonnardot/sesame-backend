4fd66250e7081b7a1f479a4597b190997758cb6b
Matthieu Cesar Bonnardot <matthieubonnardot88@gmail.com>
2021-02-03 13:25:06 +0100

    Added Registration status check

--- src/Controllers/Recognition/user.recognition.ts
+++ src/Controllers/Recognition/user.recognition.ts
@@ -1,18 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import pino from "pino";
-import { Request, Response } from "express";
-
-const logger = pino({
-  prettyPrint: true,
-});
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../../Models/Typeorm/User.entity';
 
 const verifyUserStatus = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const firstUser = await getRepository(User).findOne({
+      registrationKey: req.params.code,
+    });
+    if (!firstUser) res.send(404);
+    else if (firstUser.isActive === true) {
+      res.status(200).send('ACTIVE');
+    } else {
+      res.status(200).send('NOT ACTIVE');
+    }
+  } catch (error) {
+    res.sendStatus(501);
+  }
 };
 
 const addFaceMappings = async (req: Request, res: Response) => {
   try {
+    
   } catch (error) {}
 };
 
