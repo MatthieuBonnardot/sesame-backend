f2b07dda12498bb74d1b737a0e7662a470127fef
Matthieu Cesar Bonnardot <matthieubonnardot88@gmail.com>
2021-02-08 15:13:05 +0100

    refractored some details on issue controller

--- src/Controllers/IssueAndLogs/issue.controller.ts
+++ src/Controllers/IssueAndLogs/issue.controller.ts
@@ -1,61 +1,46 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable import/no-extraneous-dependencies */
-import pino from 'pino';
-import { Request, Response } from 'express';
-import Issues from '../../Models/Mongoose/Issues';
-
-const logger = pino({
-  prettyPrint: true,
-});
+import { Request, Response } from "express";
+import Issues from "../../Models/Mongoose/Issues";
 
 const createIssue = async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
-    const issueData: typeof Issues = req.body;
-    const createIssue = new Issues(issueData);
+    const createIssue = new Issues(req.body);
     createIssue.save().then((savedIssue) => {
       res.status(200).send(savedIssue);
     });
   } catch (error) {
     res
       .status(500)
-      .json({ error: 'an error occurred', errorMessage: error.message });
+      .json({ error: "an error occurred", errorMessage: error.message });
   }
 };
 
 const getIssues = async (_: any, res: Response) => {
   try {
-    Issues.find().then((issues) => {
-      res.status(200).send(issues);
-    });
+    Issues.find().then((issues) => res.send(issues));
   } catch (error) {
-    res.status(500).json({ error: 'an error occurred' });
+    res.status(500).json({ error: "an error occurred" });
   }
 };
 
 const toggleIssueStatus = async (req: Request, res: Response) => {
   try {
-    const filter = { _id: req.params.id };
-    const update = { active: false };
-    const opts = { new: true };
-    const doc = await Issues.findOneAndUpdate(filter, update, opts);
-    res.status(200).send(doc);
+    await Issues.findOneAndUpdate(
+      { _id: req.params.id },
+      { active: false },
+      { new: true }
+    ).then((updatedIssue) => res.send(updatedIssue));
   } catch (error) {
     res.status(501).send(error.message);
   }
 };
 
 const deleteIssue = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  Issues.findByIdAndDelete(id).then((successResponse) => {
-    if (successResponse) {
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
+  Issues.findByIdAndDelete(req.params.id).then((successResponse) => {
+    if (successResponse) res.send(`${req.params.id} has successfully been deleted`);
+    else res.sendStatus(404);
   });
 };
 
-export {
-  createIssue, getIssues, toggleIssueStatus, deleteIssue,
-};
+export { createIssue, getIssues, toggleIssueStatus, deleteIssue };
